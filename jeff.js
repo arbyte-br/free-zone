170d67273647a0afa3138d7a7f9b36ce28b9d6bf
JeffBassP <jeferson_jfs@hotmail.com>
2020-07-09 21:31:38 -0300

    Alteracao de padrao de data

--- jeff.js
+++ jeff.js
@@ -2,10 +2,12 @@ let ageCalculator = require('age-calculator');
 let rs = require('readline-sync');
 let chalk = require('chalk');
 
-let dateNasc = rs.question('Digite sua data de nascimento no padrao AAAA/MM/DD: ');
+let dateNasc = rs.question('Digite sua data de nascimento no padrao MM/DD/AAAA: ');
 
 let ageFromString = new ageCalculator.AgeFromDateString(dateNasc).age;
 
 
 
 console.log(chalk.bgYellow.black(ageFromString));
+
+// teste
